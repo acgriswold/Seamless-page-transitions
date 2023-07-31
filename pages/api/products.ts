e425424bf589c3e805a710e084a389e5dcb63e4f
acgriswold <34039420+acgriswold@users.noreply.github.com>
2023-07-31 17:19:15 -0400

    🚚: Move image resources to dedicated static images folder

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -4,7 +4,7 @@ export async function getProducts() {
       title: 'Gold Necklace',
       productId: '120499322343',
       image: {
-        src: '/../public/gold_necklace.jpg',
+        src: '/../public/static/images/gold_necklace.jpg',
         alt: 'A golden necklace in editorial styling. Rays of sunlight and pastel colors draw the attention to the necklace sitting on a minimal background.',
         width: '750',
         height: '750',
