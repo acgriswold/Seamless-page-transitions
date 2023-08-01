8b5304346d24dcf233bd5c9962e0334eb987a519
acgriswold <34039420+acgriswold@users.noreply.github.com>
2023-08-01 08:44:27 -0400

    fix(images): Fix pathing to resource images

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -13,7 +13,7 @@ export async function getProducts({
       description:
         'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
       image: {
-        src: '/../public/static/images/gold_necklace.jpg',
+        src: '/static/images/gold_necklace.jpg',
         alt: 'A golden necklace in editorial styling. Rays of sunlight and pastel colors draw the attention to the necklace sitting on a minimal background.',
         width: width,
         height: height,
@@ -27,7 +27,7 @@ export async function getProducts({
       description:
         'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
       image: {
-        src: '/../public/static/images/necklace_solo-hanging.jpg',
+        src: '/static/images/necklace_solo-hanging.jpg',
         alt: 'a golden necklace, editorial, rays of sunlight, pastel colors, professional photography, minimal background.',
         width: width,
         height: height,
@@ -41,7 +41,7 @@ export async function getProducts({
       description:
         'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
       image: {
-        src: '/../public/static/images/ring_solo_diamond.jpg',
+        src: '/static/images/ring_solo_diamond.jpg',
         alt: 'a golden necklace, editorial, rays of sunlight, pastel colors, professional photography, minimal background.',
         width: width,
         height: height,
@@ -55,7 +55,7 @@ export async function getProducts({
       description:
         'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
       image: {
-        src: '/../public/static/images/model_headshot_gold_necklace.jpg',
+        src: '/static/images/model_headshot_gold_necklace.jpg',
         alt: 'fashion jewellry, editorial, rays of sunlight, pastel colors, professional photography, minimal background.',
         width: width,
         height: height,
@@ -69,7 +69,7 @@ export async function getProducts({
       description:
         'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
       image: {
-        src: '/../public/static/images/model_framed_gold_bracelet.jpg',
+        src: '/static/images/model_framed_gold_bracelet.jpg',
         alt: 'fashion jewellry, editorial, rays of sunlight, pastel colors, professional photography, minimal background.',
         width: width,
         height: height,
@@ -83,7 +83,7 @@ export async function getProducts({
       description:
         'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
       image: {
-        src: '/../public/static/images/necklace_display_pastel.jpg',
+        src: '/static/images/necklace_display_pastel.jpg',
         alt: 'fashion jewellry, editorial, rays of sunlight, pastel colors, professional photography, minimal background.',
         width: width,
         height: height,
