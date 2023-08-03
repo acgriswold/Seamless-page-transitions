ce63a51dc790720e5b3728fac5a20829edd06961
acgriswold <34039420+acgriswold@users.noreply.github.com>
2023-08-03 17:56:56 -0400

    feat(products): Card Title and id now animate in and out

--- components/productCard.tsx
+++ components/productCard.tsx
@@ -19,6 +19,34 @@ export type ProductProps = {
 export function ProductCard(props: ProductProps) {
   const { smooth } = useTransition();
 
+  const titleVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    enter: {
+      opacity: 1,
+      transition: { delay: 0.2, ...smooth },
+    },
+    exit: {
+      opacity: 0,
+      transition: { delay: 0.1, ...smooth },
+    },
+  };
+
+  const idVariants = {
+    hidden: {
+      opacity: 0,
+    },
+    enter: {
+      opacity: 1,
+      transition: { delay: 0.3, ...smooth },
+    },
+    exit: {
+      opacity: 0,
+      transition: { delay: 0.2, ...smooth },
+    },
+  };
+
   return (
     <div className="daisy-card daisy-card-compact daisy-card-bordered bg-base-100 shadow-sm not-prose">
       <figure>
@@ -34,8 +62,10 @@ export function ProductCard(props: ProductProps) {
       </figure>
 
       <div className="prose prose-sm daisy-card-body grid gap-auto grid-cols-2 prose">
-        <strong>{props.title}</strong>
-        <div className="text-right">{props.productId}</div>
+        <motion.strong variants={titleVariants}>{props.title}</motion.strong>
+        <motion.div variants={idVariants} className="text-right">
+          {props.productId}
+        </motion.div>
       </div>
     </div>
   );
