af92c747184b462ddfd467e6871cfdb8e9824be0
acgriswold <34039420+acgriswold@users.noreply.github.com>
2023-08-04 17:28:15 -0400

    feat(useMotion): update smooth bezier curve

--- hooks/useMotion.ts
+++ hooks/useMotion.ts
@@ -1,7 +1,8 @@
 export function useTransition() {
-  const duration = 0.6;
+  const duration = 0.8;
   const snappy = { duration, ease: [0.6, 0.01, -0.05, 0.9] };
-  const smooth = { duration, ease: [0.43, 0.13, 0.23, 0.96] };
+  const smooth = { duration, ease: [0.4, 0.13, 0.44, 0.97] };
+  const smoothLegacy = { duration, ease: [0.43, 0.13, 0.23, 0.96] };
 
   return { smooth, snappy };
 }
